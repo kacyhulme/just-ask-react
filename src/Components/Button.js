c6b81ad51dd325abfcdede5966ae9fb2ff1d3727
kacy hulme <kacyhulme@gmail.com>
2019-02-27 10:02:24 -0600

    add text for button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Button = props =>  {
     return (
     <div className="Buttonstyle">
-      <Link to="/">Button Text Goes Here</Link>
+      <Link to="/">{props.buttontext}</Link>
     </div>
     )
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,13 +5,15 @@ import { Container, Row, Col } from 'reactstrap';
 
 class Main extends Component {
   render() {
+    const tryitbutton= 'try it now';
+
     return (
       <div className="Herowrapper">
        <Container>
         <Row>
           <Col md="12">
           <div>No apps, no installs. Just reminders.</div>
-          <Button />
+          <Button buttontext={tryitbutton}/>
           </Col>
           <Col md="4">A simple way to send text messages from your phone…</Col>
           <Col md="4">…to yourself or your friends and family members…</Col>
