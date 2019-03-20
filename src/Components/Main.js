f7dec4bfe6da35a93e43ced42475cf40de011c94
kacy hulme <kacyhulme@gmail.com>
2019-03-20 15:54:48 -0500

    added formatting

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import '../Styles/Main.css';
-import Navigation from './Navigation';
-import Footer from './Footer';
-import Login from './Login';
-import SignupWizard from './SignupWizard';
-import Home from './Home';
+import React, { Component } from "react";
+import "../Styles/Main.css";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import Login from "./Login";
+import SignupWizard from "./SignupWizard";
+import Home from "./Home";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {
@@ -14,43 +14,36 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink} 
-  from 'reactstrap';
+  NavLink
+} from "reactstrap";
 
 class Main extends Component {
   render() {
     return (
       <Router>
-      <div className="Main">
-        
-        <Navbar color="faded" light expand="md">
-               <NavbarBrand href="/">
-                just ask.io
-            </NavbarBrand>
+        <div className="Main">
+          <Navbar color="faded" light expand="md">
+            <NavbarBrand href="/">just ask.io</NavbarBrand>
 
             <Nav className="ml-auto" navbar>
-                <NavItem className="Login">
-                    <Link to="/login">Log In</Link>
-                </NavItem>
+              <NavItem className="Login">
+                <Link to="/login">Log In</Link>
+              </NavItem>
             </Nav>
 
             <Nav navbar>
               <NavItem className="Signup">
-                    <Link to="/signup">Sign Up</Link>
-                </NavItem>
+                <Link to="/signup">Sign Up</Link>
+              </NavItem>
             </Nav>
-        </Navbar>
+          </Navbar>
 
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignupWizard} />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignupWizard} />
 
-
-
-
-       
-        <Footer />
-      </div>
+          <Footer />
+        </div>
       </Router>
     );
   }
---
+++ src/Components/SignupWizardQuestions.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SignupWizardQuestions extends Component {
+  render() {
+    return <div>{ this.props.questions } </div>
+  }
+}
+
+export default SignupWizardQuestions;
+
